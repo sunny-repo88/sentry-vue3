11d4509f6d8db709f7541e555e19b845acdc0e3e
Khor <jing.yong.khor@intel.com>
2022-03-01 20:05:35 +0800

    add sentry

--- src/main.js
+++ src/main.js
@@ -1,9 +1,35 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+Sentry.init({
+  app,
+  dsn: "xxxx", // replace your own dsn
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
 
-app.mount('#app')
+  release: "vue3@0.0.0",
+  environment: "production",
+  initialScope: {
+    tags: { version: "vue3" },
+    user: { id: 99, email: "vue-sentry@example.com" },
+  },
+  autoSessionTracking: false,
+  logErrors: true,
+});
+
+app.use(router);
+
+app.mount("#app");
